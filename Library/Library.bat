bc30b610027356bf2c30dec30b726a77bd236e2f
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-09-05 20:40:41 +0530

    Work Files from Sep 2025

---
+++ Library/Library.bat
@@ -0,0 +1,33 @@
+::if the python uses the below default location, uncomment the below lines
+::cd C:\Users\Admin\appdata\Local\Programs\Python\Python313\Scripts
+easy_install --upgrade pip
+pip install setuptools
+pip install robotframework
+pip install robotframework-requests
+pip install robotframework-seleniumlibrary
+pip install robotframework-selenium2library
+pip install robotframework-sikuliLibrary
+pip install robotframework-AutoItLibrary
+pip install robotframework-exceldatadriver
+pip install robotframework-angularjs
+pip install robotframework-jsonlibrary
+pip install robotframework-tidy
+pip install --upgrade robotframework-seleniumlibrary
+pip install --upgrade robotframework-selenium2library
+pip install --pre --upgrade robotframework-seleniumlibrary
+pip install git+https://github.com/robotframework/SeleniumLibrary.git
+pip install wheel
+pip install selenium --upgrade
+pip install requests
+pip install jsonpath_rw
+pip install jsonpath_rw_ext
+pip install playwright
+pip install pytest
+pip install pytest-playwright
+pip install os-sys
+pip install openpyxl
+pip install browser-use
+pip install browser-use --break-system-packages
+playwright install
+playwright install-deps
+pip list
